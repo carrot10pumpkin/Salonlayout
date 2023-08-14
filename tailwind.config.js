dbd3355683742c629e0dd463fe7fdf3eb80ab6c9
jeijames <44823540+carrot10pumpkin@users.noreply.github.com>
2023-08-14 16:22:50 -0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    
+    "./node_modules/flowbite/**/*.js" ,
+    "./src/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+    require('flowbite/plugin')
+
+  ],
+}
+
